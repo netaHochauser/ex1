476a3be5ecbaffc22b60c542ccc1e30f54aef86b
netahoch <neta.hochauser@mail.huji.ac.il>
2024-11-06 23:21:12 +0200

    f

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-console.log("Hello World!");
-setupCounter();
+const textElement = document.getElementById("hoverText1");
 
-function setupCounter() {
-  let count = 0;
+// Change color when mouse is over the element
+textElement.addEventListener("mouseover", function() {
+  textElement.style.color = "orange"; // Set to desired hover color
+});
 
-  function increment() {
-    count++;
-    document.querySelector("#count").innerHTML = count;
-  }
+// Revert color when mouse leaves the element
+textElement.addEventListener("mouseout", function() {
+  textElement.style.color = "purple"; // Revert to original color
+});
 
-  function decrement() {
-    count--;
-    document.querySelector("#count").innerHTML = count;
-  }
 
-  document.querySelector("#increment").addEventListener("click", increment);
-  document.querySelector("#decrement").addEventListener("click", decrement);
-}
+// function setupCounter() {
+//   let count = 0;
+
+//   function increment() {
+//     count++;
+//     document.querySelector("#count").innerHTML = count;
+//   }
+
+//   function decrement() {
+//     count--;
+//     document.querySelector("#count").innerHTML = count;
+//   }
+
+//   document.querySelector("#increment").addEventListener("click", increment);
+//   document.querySelector("#decrement").addEventListener("click", decrement);
+// }
